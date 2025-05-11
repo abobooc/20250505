b65c3050b094e2cb5761f897eda9b5b4e5089844
abobooc <chenbozhen7@gmail.com>
2025-05-12 01:52:13 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -48,6 +48,28 @@ function draw() {
           noStroke();
           circle(keypoint.x, keypoint.y, 16);
         }
+
+        // Function to draw lines between keypoints
+        function drawLines(startIndex, endIndex, color) {
+          stroke(...color);
+          strokeWeight(2);
+          for (let i = startIndex; i < endIndex; i++) {
+            let start = hand.keypoints[i];
+            let end = hand.keypoints[i + 1];
+            line(start.x, start.y, end.x, end.y);
+          }
+        }
+
+        // Define color based on handedness
+        let color = hand.handedness == "Left" ? [255, 0, 255] : [255, 255, 0];
+
+        // Draw lines for each segment
+        if (hand.keypoints.length > 4) drawLines(0, 4, color);  // Keypoints 0 to 4
+        if (hand.keypoints.length > 8) drawLines(5, 8, color);  // Keypoints 5 to 8
+        if (hand.keypoints.length > 12) drawLines(9, 12, color); // Keypoints 9 to 12
+        if (hand.keypoints.length > 16) drawLines(13, 16, color); // Keypoints 13 to 16
+        if (hand.keypoints.length > 20) drawLines(17, 20, color); // Keypoints 17 to 20
+        
       }
     }
   }
